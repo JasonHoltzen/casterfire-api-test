bc3f31c1bc96a7f91e991911a7f7b48bf76e9614
Jason Holtzen <mystikaljester@gmail.com>
2022-05-04 09:03:11 -0500

    updated meta info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,10 @@
 	import SpellViewer from '$components/spells/spellViewer.svelte';
 </script>
 
+<svelte:head>
+<title>CasterFire | Pathfinder 2e Spell Book</title>
+</svelte:head>
+
 <FilterBar2 />
 <SpellSelector />
 <SpellViewer />
